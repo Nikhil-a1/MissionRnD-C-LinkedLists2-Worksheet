ed91b0e19c7f4a3d82876afeef62ac1498143489
Nikhil-a1 <Nikhil-a1@missionrnd.com>
2016-03-23 18:09:19 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -10,14 +10,111 @@ ERROR CASES: Return NULL for error cases.
 
 NOTES:
 */
-
+//time complexity:O(n)
+//algorithm: merge algorithm
 #include <stdio.h>
-
+#include<stdlib.h>
 struct node {
 	int num;
 	struct node *next;
 };
 
 struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+
+	if (head1 == NULL && head2 == NULL)
+		return NULL;
+	else if (head1 == NULL && head2 != NULL)
+		return head2;
+	else if (head2 == NULL && head1 != NULL)
+		return head1;
+	else
+	{
+		int start = 0, k = 0, len1 = 0, len2 = 0, count = 0;
+		struct node* head, *temp1, *temp2, *link, *temp;
+		temp1 = head1;
+		temp2 = head2;
+		while (temp1)
+		{
+			len1++;
+			temp1 = temp1->next;
+		}
+		temp1 = head1;
+		while (temp2)
+		{
+			len2++;
+			temp2 = temp2->next;
+		}
+		temp2 = head2;
+		while (start < len1 && k < len2)
+		{
+			if (temp1->num >= temp2->num)
+			{
+				if (count == 0)
+				{
+					head = (struct node*)malloc(sizeof(struct node));
+					temp = head;
+					temp->num = temp2->num;
+					temp->next = NULL;
+					temp2 = temp2->next;
+					count++;
+				}
+				else
+				{
+					link = (struct node*)malloc(sizeof(struct node));
+					link->num = temp2->num;
+					temp->next = link;
+					temp = link;
+					temp2 = temp2->next;
+				}
+				k++;
+			}
+			else
+			{
+				if (count == 0)
+				{
+					head = (struct node*)malloc(sizeof(struct node));
+					temp = head;
+					temp->num = temp1->num;
+					temp->next = NULL;
+					temp1 = temp1->next;
+					count++;
+				}
+				else
+				{
+					link = (struct node*)malloc(sizeof(struct node));
+					link->num = temp1->num;
+					temp->next = link;
+					temp = link;
+					temp1 = temp1->next;
+				}
+				start++;
+			}
+		}
+		if (start >= len1)
+		{
+			for (; k < len2; k++)
+			{
+				link = (struct node*)malloc(sizeof(struct node));
+				link->num = temp2->num;
+				temp->next = link;
+				temp = link;
+				temp2 = temp2->next;
+			}
+			temp->next = NULL;
+		}
+		else if (k >= len2)
+		{
+			for (; start < len1; start++)
+			{
+				link = (struct node*)malloc(sizeof(struct node*));
+				link->num = temp1->num;
+				temp->next = link;
+				temp = link;
+				temp1 = temp1->next;
+			}
+			temp->next = NULL;
+		}
+		return head;
+	}
 }
+
